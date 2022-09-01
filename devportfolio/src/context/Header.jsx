034a7518e1c0de36997575b1fdfd03fb916e5fc3
camcoem <94968301+camcoem@users.noreply.github.com>
2022-09-01 21:54:59 +0200

    styling header added new font and text

--- devportfolio/src/context/Header.jsx
+++ devportfolio/src/context/Header.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Header() {
   return (
     <div>
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h1> Hi, I'm Camila</h1>
+        <h2>a Front-End Developer</h2>
+      </header>
     </div>
   )
 }
