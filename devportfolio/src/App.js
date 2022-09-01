460742f111a2ce10724badb2072ada3906fcc7ab
camcoem <94968301+camcoem@users.noreply.github.com>
2022-09-01 21:42:00 +0200

    added header component

--- devportfolio/src/App.js
+++ devportfolio/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./context/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div>
+      <Header />
     </div>
   );
 }
