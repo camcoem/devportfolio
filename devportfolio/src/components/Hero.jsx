40869013224f65eaf2813f26daddf09c84d4aede
camcoem <94968301+camcoem@users.noreply.github.com>
2022-09-08 14:04:21 +0200

    styling hero component, added span

--- devportfolio/src/components/Hero.jsx
+++ devportfolio/src/components/Hero.jsx
@@ -5,7 +5,8 @@ export default function Hero() {
   return (
     <>
     <Header />
-       <h1>Coding web</h1><h1 className="titleExperience">experiences</h1>
+
+       <h1>Coding web<span className="titleExperience"> experiences</span></h1>
        {/* <p>I'm Camila, Front-End Developer and coffee lover </p> */}
        <button className='btn-primary'>Let's create</button>
     </>
