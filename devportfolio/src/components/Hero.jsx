59fb6d9aca2feddee5502fa9f0ebc68d456770d9
camcoem <94968301+camcoem@users.noreply.github.com>
2022-09-08 13:26:38 +0200

    imported font bodoni moda and changed H1 in hero

--- devportfolio/src/components/Hero.jsx
+++ devportfolio/src/components/Hero.jsx
@@ -5,9 +5,9 @@ export default function Hero() {
   return (
     <>
     <Header />
-       <h1> Hi, I'm Camila</h1>
-       <h2>Front-End Developer</h2>
-       <button className='btn-primary'>Let's create together</button>
+       <h1>Coding web</h1><h1 className="titleExperience">experiences</h1>
+       {/* <p>I'm Camila, Front-End Developer and coffee lover </p> */}
+       <button className='btn-primary'>Let's create</button>
     </>
   )
 }
