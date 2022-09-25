26d4093c13ad5d863d0328263392bb958440c1d1
camcoem <94968301+camcoem@users.noreply.github.com>
2022-09-25 13:15:56 +0200

    added flexbox in hero section + cleanuup code

--- devportfolio/src/components/Hero.jsx
+++ devportfolio/src/components/Hero.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function Hero() {
   return (
-    <>
+    <div className="hero-div">
       <h1>
         Coding web<span className="titleExperience"> experiences</span>
       </h1>
       {/* <p>I'm Camila, Front-End Developer and coffee lover </p> */}
       <button className="btn-primary">Let's create</button>
-    </>
+    </div>
   );
 }
