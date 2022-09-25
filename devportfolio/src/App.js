30a781a393eff7e91d6f6ed144c90abe4c75f388
camcoem <94968301+camcoem@users.noreply.github.com>
2022-09-25 19:54:10 +0200

    added components contact, projects and aboutme

--- devportfolio/src/App.js
+++ devportfolio/src/App.js
@@ -8,6 +8,9 @@ import "./fonts/YouDense-Glass-BlowRegular.ttf";
 //Components
 import Hero from "./components/Hero";
 import Header from "./components/Header";
+import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -15,6 +18,9 @@ function App() {
       <div className="gradient-background">
         <Header />
         <Hero />
+        <AboutMe />
+        <Projects />
+        <Contact />
       </div>
     </>
   );
