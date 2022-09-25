bfbc107977367d3886c4625b553c8a84cb9ec2b1
camcoem <94968301+camcoem@users.noreply.github.com>
2022-09-25 13:43:39 +0200

    styled hero titel

--- devportfolio/src/components/Hero.jsx
+++ devportfolio/src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Hero() {
   return (
     <div className="hero-div">
-      <h1>
+      <h1 className="hero-titel">
         Coding web<span className="titleExperience"> experiences</span>
       </h1>
       <button className="btn-primary">Let's create</button>
