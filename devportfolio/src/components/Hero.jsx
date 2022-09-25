bc65cc8abba9e195e651b287c924cd1edef334c9
camcoem <94968301+camcoem@users.noreply.github.com>
2022-09-25 13:27:33 +0200

    styled btn-primary

--- devportfolio/src/components/Hero.jsx
+++ devportfolio/src/components/Hero.jsx
@@ -6,7 +6,6 @@ export default function Hero() {
       <h1>
         Coding web<span className="titleExperience"> experiences</span>
       </h1>
-      {/* <p>I'm Camila, Front-End Developer and coffee lover </p> */}
       <button className="btn-primary">Let's create</button>
     </div>
   );
