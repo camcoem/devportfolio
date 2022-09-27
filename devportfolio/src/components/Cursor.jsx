896e5e04540d06a7a4334792d62c8aab54d46d2d
camcoem <94968301+camcoem@users.noreply.github.com>
2022-09-27 18:11:41 +0200

    styled

--- devportfolio/src/components/Cursor.jsx
+++ devportfolio/src/components/Cursor.jsx
@@ -1,11 +1,23 @@
-import React from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Cursor() {
   const [mousePosition, setMousePosition] = useState({
     x: 0,
     y: 0,
   });
+  console.log(mousePosition);
+
+  useEffect(() => {
+    const mouseMove = (e) => {
+      console.log(e);
+    };
+
+    window.addEventListener("mouseMove ", mouseMove);
+
+    return () => {
+      window.removeEventListener(" mousemove ", mouseMove);
+    };
+  }, []);
 
   return <div className="cursor"></div>;
 }
