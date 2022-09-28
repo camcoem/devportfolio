d8ceac1dd3aa23b84c2361dc1e5d8814fea1fa81
camcoem <94968301+camcoem@users.noreply.github.com>
2022-09-28 10:58:54 +0200

    added h1 to aboutMe component

--- devportfolio/src/components/AboutMe.jsx
+++ devportfolio/src/components/AboutMe.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function AboutMe() {
-  return <div></div>;
+  return (
+    <div>
+      <h3 className="title">A bit about me </h3>
+    </div>
+  );
 }
