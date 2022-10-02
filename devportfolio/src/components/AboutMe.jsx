0c72a1b5951da97d29e5e279e8ec15e29d6e5fbb
camcoem <94968301+camcoem@users.noreply.github.com>
2022-10-02 20:40:07 +0200

    added new div and styled as glassmorphism

--- devportfolio/src/components/AboutMe.jsx
+++ devportfolio/src/components/AboutMe.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function AboutMe() {
   return (
-    <div>
+    <div className="aboutMeDiv">
       <h3 className="title">A bit about me </h3>
     </div>
   );
