65252bf0c3e2ca9212acc3a1c5a36d1506f8390b
camcoem <94968301+camcoem@users.noreply.github.com>
2022-10-02 20:00:46 +0200

    ledited duration of animation

--- devportfolio/src/components/Hero.jsx
+++ devportfolio/src/components/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
   const props = useSpring({
     from: { opacity: 0 },
     to: { opacity: 1 },
-    config: { delay: 5000, duration: 2000 },
+    config: { delay: 10000, duration: 4000 },
   });
 
   return (
