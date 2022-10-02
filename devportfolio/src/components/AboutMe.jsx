2614d0d506190b54adaa176978bf230a8e5c70d2
camcoem <94968301+camcoem@users.noreply.github.com>
2022-10-02 20:51:02 +0200

    added new structure to component

--- devportfolio/src/components/AboutMe.jsx
+++ devportfolio/src/components/AboutMe.jsx
@@ -3,7 +3,10 @@ import React from "react";
 export default function AboutMe() {
   return (
     <div className="aboutMeDiv">
-      <h3 className="title">A bit about me </h3>
+      <div>
+        <h3 className="title">A bit about me </h3>
+      </div>
+      <div></div>
     </div>
   );
 }
