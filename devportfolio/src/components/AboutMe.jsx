e4d1c425f617c75a94f73f93492ac76e259e6bb1
camcoem <94968301+camcoem@users.noreply.github.com>
2022-10-02 21:15:05 +0200

    added titel and style aboutMeTitel

--- devportfolio/src/components/AboutMe.jsx
+++ devportfolio/src/components/AboutMe.jsx
@@ -2,11 +2,27 @@ import React from "react";
 
 export default function AboutMe() {
   return (
-    <div className="aboutMeDiv">
-      <div className="leftSide">
-        <h3 className="title">A bit about me </h3>
+    <>
+      <h1 className="aboutMeTitel">about me</h1>
+      <div className="aboutMeDiv">
+        <div className="leftSide">
+          <h3 className="title">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+            quod, sequi obcaecati ab, praesentium facilis illo minima nihil
+            veritatis voluptate temporibus exercitationem corporis distinctio
+            laboriosam earum voluptas animi pariatur vero quam voluptatem
+            delectus est quas vel mollitia? Molestiae error accusantium sint
+            mollitia quisquam debitis minus deserunt, laboriosam sit! Ullam
+            perspiciatis suscipit optio sapiente, incidunt ad a non, reiciendis
+            autem maxime dolorem similique eius atque ratione et consectetur,
+            officia error eaque rerum in ut. Dolorem aspernatur consequatur, hic
+            quis nam, maxime amet harum, esse facere blanditiis recusandae odit
+            doloribus voluptatem beatae quaerat sit corporis provident iure
+            totam minus fuga laborum neque?
+          </h3>
+        </div>
+        <div className="rightSide"></div>
       </div>
-      <div className="rightSide"></div>
-    </div>
+    </>
   );
 }
