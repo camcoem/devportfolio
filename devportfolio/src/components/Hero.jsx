2b7587a340c8be041a66ba6ec322ad981aefb594
camcoem <94968301+camcoem@users.noreply.github.com>
2022-10-02 19:44:34 +0200

    added useSprng api fully functioning

--- devportfolio/src/components/Hero.jsx
+++ devportfolio/src/components/Hero.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { useTransition, animated } from "react-spring";
+import { useSpring, animated } from "react-spring";
 
 export default function Hero() {
+  const props = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    config: { delay: 3000, duration: 2000 },
+  });
+
   return (
     <div className="hero-div">
       <h1 className="hero-titel">
         Coding web
-        <animated.span className="titleExperience"> experiences</animated.span>
+        <animated.span className="titleExperience" style={props}>
+          {" "}
+          experiences
+        </animated.span>
       </h1>
       <button className="btn-primary">Let's create</button>
     </div>
