3590f72fb2562497f8837a1cdb21652b996ecf85
camcoem <94968301+camcoem@users.noreply.github.com>
2022-11-25 00:28:24 +0100

    added div and form

--- devportfolio/src/components/Contact.jsx
+++ devportfolio/src/components/Contact.jsx
@@ -3,21 +3,34 @@ import React from "react";
 export default function Contact() {
   return (
     <section>
-      <h2> Contact me</h2>
-      <label>
-        Name
-        <input type="text" name="name" />
-      </label>
-      <label>
-        Email
-        <input type="email" name="user_email" placeholder="your@email.com" />
-      </label>
-      <label>
-        Subject
-        <input type="text" name="subject" placeholder="Email subject" />
-      </label>
-      <textarea name="message" placeholder="Your message" cols={30} rows={10} />
-      <button type="submit">Send Message</button>
+      <h2 className="contact-me"> Contact me</h2>
+      <div className="container-contact">
+        <form>
+          <label>
+            Name
+            <input type="text" name="name" />
+          </label>
+          <label>
+            Email
+            <input
+              type="email"
+              name="user_email"
+              placeholder="your@email.com"
+            />
+          </label>
+          <label>
+            Subject
+            <input type="text" name="subject" placeholder="Email subject" />
+          </label>
+          <textarea
+            name="message"
+            placeholder="Your message"
+            cols={30}
+            rows={10}
+          />
+          <button type="submit">Send Message</button>
+        </form>
+      </div>
     </section>
   );
 }
