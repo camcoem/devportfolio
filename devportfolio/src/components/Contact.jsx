abcf52ad1b5660178c20881e564a00c7c6c178b6
camcoem <94968301+camcoem@users.noreply.github.com>
2022-11-26 14:48:27 +0100

    fixed a tag - now opening in a new tab github icon

--- devportfolio/src/components/Contact.jsx
+++ devportfolio/src/components/Contact.jsx
@@ -60,18 +60,14 @@ export default function Contact() {
           </form>
         </div>
         <div className="footer">
-          <a href="https://github.com/camcoem">
+          <a href="https://github.com/camcoem" target="_blank">
             <i className="devicon-github-original" id="iconFooter"></i>
           </a>
           <a
             href="https://www.linkedin.com/in/camila-coello-merino/"
             target="_blank"
           >
-            <i
-              className="devicon-linkedin-plain"
-              id="iconFooter"
-              target="_blank"
-            ></i>
+            <i className="devicon-linkedin-plain" id="iconFooter"></i>
           </a>
         </div>
       </div>
