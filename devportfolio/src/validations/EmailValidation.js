aac6fe0f977202a9af0808219acf6269436e559c
camcoem <94968301+camcoem@users.noreply.github.com>
2022-11-28 16:21:40 +0100

    built emailSchema and exported it

--- devportfolio/src/validations/EmailValidation.js
+++ devportfolio/src/validations/EmailValidation.js
@@ -1,1 +1,7 @@
 import * as yup from "yup";
+
+export const emailSchema = yup.object().shape({
+  name: yup.string().required(),
+  email: yup.string().email().required(),
+  text: yup.string().required(),
+});
