37d5406efe4245a29bdae83475a82ef54e30965d
camcoem <94968301+camcoem@users.noreply.github.com>
2022-11-28 16:25:29 +0100

    added subject to validation schema

--- devportfolio/src/validations/EmailValidation.js
+++ devportfolio/src/validations/EmailValidation.js
@@ -3,5 +3,6 @@ import * as yup from "yup";
 export const emailSchema = yup.object().shape({
   name: yup.string().required(),
   email: yup.string().email().required(),
+  subject: yup.string(),
   text: yup.string().required(),
 });
