08eafbf92abcb2c2c51d39a3b7dcafd72f7e4355
camcoem <94968301+camcoem@users.noreply.github.com>
2022-12-02 14:50:31 +0100

    added all project cards with names

--- devportfolio/src/components/Projects.jsx
+++ devportfolio/src/components/Projects.jsx
@@ -5,9 +5,59 @@ export default function Projects() {
     <div>
       <h2 className="contactMe"> Projects</h2>
       <div className="project-cards">
-        <div className="card-1">
+        <div className="cards">
           <div className="imgCard"></div>
           <h2>ToDo App</h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s
+          </p>
+          <div className="readmore">
+            <span className="spanBtn">
+              <a href="#" className="readMoreLink">
+                <button className="btn">Read more</button>
+              </a>
+            </span>
+          </div>
+        </div>
+
+        <div className="cards">
+          <div className="imgCard"></div>
+          <h2>MemeYourself</h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s
+          </p>
+          <div className="readmore">
+            <span className="spanBtn">
+              <a href="#" className="readMoreLink">
+                <button className="btn">Read more</button>
+              </a>
+            </span>
+          </div>
+        </div>
+
+        <div className="cards">
+          <div className="imgCard"></div>
+          <h2>The Bean Machine</h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s
+          </p>
+          <div className="readmore">
+            <span className="spanBtn">
+              <a href="#" className="readMoreLink">
+                <button className="btn">Read more</button>
+              </a>
+            </span>
+          </div>
+        </div>
+        <div className="cards">
+          <div className="imgCard"></div>
+          <h2>thebarista.dev Blog</h2>
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
