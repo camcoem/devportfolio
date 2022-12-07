b760f9c8c3923534296524fc36f0a8bfe827a0b3
camcoem <94968301+camcoem@users.noreply.github.com>
2022-12-08 00:02:25 +0100

    imported and implemented useState

--- devportfolio/src/components/FlippableCard.jsx
+++ devportfolio/src/components/FlippableCard.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Card from "./Card";
+import { useState } from "react";
 
 export default function FlippableCard() {
+  const [showFront, setShowFront] = useState(true);
   return (
     <div className="flippableCardContainer">
       <Card />
