87d8028a8dfa50148ced00cd94329408e2603468
camcoem <94968301+camcoem@users.noreply.github.com>
2022-12-07 23:57:17 +0100

    renamed card component

---
+++ devportfolio/src/components/Card.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Card(props) {
+  return (
+    <>
+      <div className="cards">
+        <div className="imgCardTodo"></div>
+        <h2>{props.name}</h2>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s
+        </p>
+        <div className="readmore">
+          <span className="spanBtn">
+            <a href="#" className="readMoreLink">
+              <button className="btn">Read more</button>
+            </a>
+          </span>
+        </div>
+      </div>
+    </>
+  );
+}
