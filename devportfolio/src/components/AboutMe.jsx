33cd6e388a2554cb3f146e968862cbb87e945e48
camcoem <94968301+camcoem@users.noreply.github.com>
2023-01-19 19:35:47 +0100

    added h3 and div surrounding grid

--- devportfolio/src/components/AboutMe.jsx
+++ devportfolio/src/components/AboutMe.jsx
@@ -49,90 +49,93 @@ export default function AboutMe() {
             </p>
           </div>
         </div>
-        <div className="rightSide">
-          <i
-            className="devicon-javascript-plain"
-            id="aboutMeIcon"
-            data-hover="JavaScript"
-          ></i>
-          <i
-            className="devicon-html5-plain"
-            id="aboutMeIcon"
-            data-hover="HTML5"
-          ></i>
-          <i
-            className="devicon-css3-plain"
-            id="aboutMeIcon"
-            data-hover="CSS"
-          ></i>
-          <i
-            className="devicon-react-original"
-            id="aboutMeIcon"
-            data-hover="React"
-          ></i>
+        <div className="aboutMeStack">
+          <h3 className="whoIsTitel">What I can do:</h3>
+          <div className="rightSide">
+            <i
+              className="devicon-javascript-plain"
+              id="aboutMeIcon"
+              data-hover="JavaScript"
+            ></i>
+            <i
+              className="devicon-html5-plain"
+              id="aboutMeIcon"
+              data-hover="HTML5"
+            ></i>
+            <i
+              className="devicon-css3-plain"
+              id="aboutMeIcon"
+              data-hover="CSS"
+            ></i>
+            <i
+              className="devicon-react-original"
+              id="aboutMeIcon"
+              data-hover="React"
+            ></i>
 
-          <i
-            className="devicon-python-plain"
-            id="aboutMeIcon"
-            data-hover="Python"
-          ></i>
-          <i
-            className="devicon-npm-original-wordmark"
-            id="aboutMeIcon"
-            data-hover="npm"
-          ></i>
-          <i
-            className="devicon-nodejs-plain-wordmark"
-            id="aboutMeIcon"
-            data-hover="NodeJS"
-          ></i>
-          <i
-            className="devicon-express-original-wordmark"
-            id="aboutMeIcon"
-            data-hover="express"
-          ></i>
+            <i
+              className="devicon-python-plain"
+              id="aboutMeIcon"
+              data-hover="Python"
+            ></i>
+            <i
+              className="devicon-npm-original-wordmark"
+              id="aboutMeIcon"
+              data-hover="npm"
+            ></i>
+            <i
+              className="devicon-nodejs-plain-wordmark"
+              id="aboutMeIcon"
+              data-hover="NodeJS"
+            ></i>
+            <i
+              className="devicon-express-original-wordmark"
+              id="aboutMeIcon"
+              data-hover="express"
+            ></i>
 
-          <i
-            className="devicon-github-original-wordmark"
-            id="aboutMeIcon"
-            data-hover="GitHub"
-          ></i>
-          <i
-            className="devicon-git-plain"
-            id="aboutMeIcon"
-            data-hover="git"
-          ></i>
-          <i
-            className="devicon-vscode-plain"
-            id="aboutMeIcon"
-            data-hover="Visual Studio Code"
-          ></i>
-          <i
-            className="devicon-sass-original"
-            id="aboutMeIcon"
-            data-hover="SASS"
-          ></i>
+            <i
+              className="devicon-github-original-wordmark"
+              id="aboutMeIcon"
+              data-hover="GitHub"
+            ></i>
+            <i
+              className="devicon-git-plain"
+              id="aboutMeIcon"
+              data-hover="git"
+            ></i>
+            <i
+              className="devicon-vscode-plain"
+              id="aboutMeIcon"
+              data-hover="Visual Studio Code"
+            ></i>
+            <i
+              className="devicon-sass-original"
+              id="aboutMeIcon"
+              data-hover="SASS"
+            ></i>
 
-          <i
-            className="devicon-figma-plain"
-            id="aboutMeIcon"
-            data-hover="Figma"
-          ></i>
-          <i
-            className="devicon-tailwindcss-original-wordmark"
-            id="aboutMeIcon"
-            data-hover="Tailwind CSS"
-          ></i>
-          <i
-            className="devicon-bootstrap-plain"
-            id="aboutMeIcon"
-            data-hover="Bootstrap"
-          ></i>
-          <i
-            className="devicon-firebase-plain"
-            id="aboutMeIcon"
-            data-hover="Firebase"
-          ></i>
+            <i
+              className="devicon-figma-plain"
+              id="aboutMeIcon"
+              data-hover="Figma"
+            ></i>
+            <i
+              className="devicon-tailwindcss-original-wordmark"
+              id="aboutMeIcon"
+              data-hover="Tailwind CSS"
+            ></i>
+            <i
+              className="devicon-bootstrap-plain"
+              id="aboutMeIcon"
+              data-hover="Bootstrap"
+            ></i>
+            <i
+              className="devicon-firebase-plain"
+              id="aboutMeIcon"
+              data-hover="Firebase"
+            ></i>
+          </div>
         </div>
       </div>
     </>
